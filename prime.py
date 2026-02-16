d6cf7cbf1c2eb055fcddae14372bf179ccd2bfd2
Sukhveer Dhaliwal <kaursukhveer21@gmail.com>
2026-02-16 12:04:56 -0500

    Step 4 - Returns [3]
    Assert that the number 3 generates a list with [3]

--- prime.py
+++ prime.py
@@ -14,6 +14,10 @@ def test_non_integer_raises_ValueError():
 def test_1_Returns_Empty_List():
     """Step 2: Assert that the number generates an Empty List[]"""
     assert generate_prime_factors(1) == []
+def test_3_Returns_list_with_3():
+    """Step 4: Assert that the number 3 generates a list with 3 [3]"""
+    assert generate_prime_factors(3) == [3]
+
 def generate_prime_factors(n):
     if not isinstance(n, int):
         raise ValueError('Input must be an integer')
@@ -22,4 +26,12 @@ def generate_prime_factors(n):
         while n % 2 == 0:
             factors.append(2)
             n //= 2
+    return factors    divisor = 2
+
+    while n >1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+
     return factors
